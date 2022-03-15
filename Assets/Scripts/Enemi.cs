9f6f18ddf609c36209d3bf44c0d3d7ad88868bd8
Remaken <jorgeazevedo24@gmail.com>
2022-03-15 10:28:18 +0100

    Alpha0.05

--- Assets/Scripts/Enemi.cs
+++ Assets/Scripts/Enemi.cs
@@ -1,10 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemi : MonoBehaviour
 {
     private int _hitPoints = 10;
     
     //Todo: raycast detecte joueur
+
+    private void Update()
+    {
+        RaycastHit hit;
+       // if (Physics.Raycast((transform.position)))
+        {
+            
+        }
+    }
 }
--- Assets/Scripts/Joeur.cs
+++ Assets/Scripts/Joeur.cs
@@ -12,6 +12,7 @@ public class Joeur : MonoBehaviour
    }
 
 
+   
 
    private void PlayerMouvement()
    {
